97e54ce7c7b4e0d0604ec18e9da008565ad46ecd
W-Kraxcker <86090853+W-Kraxcker@users.noreply.github.com>
2022-05-04 22:39:57 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Navapp from './mainnav';
+import Home from './page/index'
+import Features from './page/features';
+import Pricing from './page/pricing';
+import RegisLog from './page/regisandlog/regislog';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navapp/>
+    
+    <Routes>
+      <Route exact path='/' element={<Home />}></Route>
+      <Route path='/features' element={<Features/>}></Route>
+      <Route path='/pricing' element={<Pricing/>}></Route>
+      <Route path='/registerandlogin' element={<RegisLog/>}></Route>
+    </Routes>
+    
+  </BrowserRouter>
+  );
+}
+  
+export default App;
